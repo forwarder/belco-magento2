7daaf962d9d949787b6141de1b2095439c8a7018
Eelco Wiersma <contact@pagebakers.nl>
2019-10-10 10:28:11 +0200

    Widget beta snippet

--- view/frontend/web/js/view/belco-config.js
+++ view/frontend/web/js/view/belco-config.js
@@ -5,19 +5,8 @@ define([
   'use strict';
 
   function loadWidget(callback) {
-    var s = document.createElement('script');
-    s.async = true;
-    s.src = 'https://cdn.belco.io/widget.min.js';
-    s.onload = s.onreadystatechange = function() {
-      var rs = this.readyState;
-      if (rs) 
-        if (rs != 'complete')
-          if (rs != 'loaded') return;
-
-      callback();
-    };
-    var x = document.getElementsByTagName('script')[0];
-    x.parentNode.insertBefore(s, x);
+    !function(n,o){var e=window.belcoFunction||"Belco";window[e]||(window[e]=function(n){if(void 0===window[e][n])throw new Error("Unknown method");return window[e][n].apply(window[e],Array.prototype.slice.call(arguments,1))}),window[e]._q=[];for(var i=["init","sync","track","page","open","close","toggle","on","once","off","anonymousId","customer","reset","sendMessage"],t=function(n){return function(){var o=Array.prototype.slice.call(arguments);return o.unshift(n),window[e]._q.push(o),window[e]}},w=0;w<i.length;w++){var r=i[w];window[e][r]=t(r)}window[e].load=function(e){if(!n.getElementById("belco-js")){var i=n.createElement(o);i.async=!0,i.id="belco-js",i.type="text/javascript",i.src="//cdn.belco.io/v2/widget.js",i.onload=function(n){"function"==typeof e&&e(n)};var t=n.getElementsByTagName(o)[0];t.parentNode.insertBefore(i,t)}},window.belcoConfig&&window[e].load(function(){window[e]("init",window.belcoConfig)})}(document,"script");
+    Belco.load(callback)
   }
 
   function initBelco(config) {
